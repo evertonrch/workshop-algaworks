3c41e6b55ba375b5eb441c520e263481ae259600
Everton Rocha <irocha10@hotmail.com>
2022-01-17 23:51:16 -0300

    Service, ExceptionHandler

---
+++ src/main/java/com/algaworks/logapi/domain/exception/DomainException.java
@@ -0,0 +1,10 @@
+package com.algaworks.logapi.domain.exception;
+
+public class DomainException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DomainException(String msg) {
+		super(msg);
+	}
+}
