86b86676af4145043cefbda6deb67a416c82d429
Everton Rocha <irocha10@hotmail.com>
2022-01-17 21:32:47 -0300

    ResponseEntity, RequestMapping

--- src/main/java/com/algaworks/logapi/controller/ClienteController.java
+++ src/main/java/com/algaworks/logapi/controller/ClienteController.java
@@ -3,24 +3,33 @@ package com.algaworks.logapi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.logapi.domain.model.Cliente;
+import com.algaworks.logapi.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	@Autowired
+	private ClienteRepository repository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {		
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return repository.findAll();
+	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		return repository.findById(clienteId)
+					.map(ResponseEntity::ok)
+					.orElse(ResponseEntity.notFound().build());
 	}
 	
 }
--- src/main/java/com/algaworks/logapi/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/logapi/domain/repository/ClienteRepository.java
@@ -1,7 +1,15 @@
 package com.algaworks.logapi.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.algaworks.logapi.domain.model.Cliente;
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	List<Cliente> findByNome(String nome);
+	List<Cliente> findByNomeContaining(String nome);
 }
